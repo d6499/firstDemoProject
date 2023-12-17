3cdc4012b2b3f3d443db91ab2e33b0a2ad9f7980
d6499 <dinesshpatil77@gmail.com>
2023-12-17 18:16:11 +0530

    first project committed

---
+++ src/test/java/com/project1/gitProject/GitProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project1.gitProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
